21e19e499d6a493200e3c318abee04a12407045b
Selepe Sello <sbkskhalo.kq@gmail.com>
2023-09-16 21:11:41 +0200

    Update: Added more Events to the JSON Array

--- health-app/src/components/Data/EventsData.jsx
+++ health-app/src/components/Data/EventsData.jsx
@@ -36,8 +36,8 @@ const EventsData = [
       name: 'Michael Foster',
       imageUrl:
         'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      startDatetime: '2022-06-16T13:00',
-      endDatetime: '2022-06-16T14:30',
+      startDatetime: '2023-06-16T13:00',
+      endDatetime: '2023-06-16T14:30',
     },
     {
       id: 6,
@@ -68,8 +68,8 @@ const EventsData = [
       name: 'Leslie Alexander',
       imageUrl:
         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      startDatetime: '2023-09-16T13:00',
-      endDatetime: '2023-09-16T14:30',
+      startDatetime: '2023-09-17T13:00',
+      endDatetime: '2023-09-17T14:30',
     },
     {
       id: 10,
