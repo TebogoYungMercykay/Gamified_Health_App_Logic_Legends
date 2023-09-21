20ac85c7cfc8995d6760f36a5241ad875c0d7d58
tumelo4 <tmosomane4@gmail.com>
2023-09-22 01:49:38 +0200

    Partial Profile Modification

--- health-app/app/components/settings/Profile.tsx
+++ health-app/app/components/settings/Profile.tsx
@@ -4,9 +4,8 @@ const Profile = () => {
     return (
         <div className=' w-full flex flex-col gap-6 items-center'>
             <div className=' w-72 h-72 rounded-full bg-red-700 overflow-hidden'></div>
-            {/*  Name  */}
-            <h3>Tumi</h3>
-            <button className=' bg-blue-500 p-3 text-white rounded-sm mb-7'>Edit Profile</button>
+            <h2>Tumi</h2>
+            <button className=' bg-blue-500 py-3 px-6 text-white rounded-3xl mb-7'>Edit Profile</button>
         </div>
     )
 }
