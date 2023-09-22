5a174674dd6ec437d029abbcd708b3a3063e7379
TebogoYungMercykay <sbkskhalo.kq@gmail.com>
2023-09-22 11:27:35 +0200

    Updated the Profile icon and the App icon

--- health-app/app/components/settings/Profile.tsx
+++ health-app/app/components/settings/Profile.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import { IoPersonCircleOutline } from 'react-icons/io5';
 
 const Profile = () => {
     return (
         <div className=' w-full flex flex-col gap-6 items-center'>
-            <div className=' w-72 h-72 rounded-full bg-red-700 overflow-hidden'></div>
-            <h2>Tumi</h2>
+            <div className='w-72 h-72 min-w-[2.5rem] rounded-full'>
+                <IoPersonCircleOutline
+                    className="w-full h-full text-gray-500"
+                    size={100}
+                />
+            </div>
+            <h2>Tumi Khumalo</h2>
             <button className=' bg-blue-500 py-3 px-6 text-white rounded-3xl mb-7'>Edit Profile</button>
         </div>
     )
 }
+
 export default Profile
